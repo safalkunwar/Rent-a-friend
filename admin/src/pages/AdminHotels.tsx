d4d97d67d7c97241b881a693b33bd626c7185b19
Safal Kunwar <129306053+safalkunwar@users.noreply.github.com>
2026-08-14 08:04:10 +0545

    refactor: remove admin panel code from main branch

--- admin/src/pages/AdminHotels.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AdminVenues } from './AdminVenues';
-
-export function AdminHotels() {
-  return <AdminVenues type="hotels" />;
-}
--- admin/src/pages/AdminRestaurants.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AdminVenues } from './AdminVenues';
-
-export function AdminRestaurants() {
-  return <AdminVenues type="restaurants" />;
-}
